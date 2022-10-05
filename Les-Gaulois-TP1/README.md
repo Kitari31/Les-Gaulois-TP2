7fa4a5e44c731ed883d6a0c674abd94bc9b88a4b
Kitari31 <76065071+Kitari31@users.noreply.github.com>
2022-10-05 15:26:25 +0200

    Add files via upload

---
+++ Les-Gaulois-TP1/README.md
@@ -0,0 +1,2 @@
+# Les-Gaulois-TP1
+Depot de dossier sur le TP1 de ILU1
---
+++ Les-Gaulois-TP1/src/histoire/Scénario.java
@@ -0,0 +1,24 @@
+package histoire;
+
+import personnages.Gaulois;
+import personnages.Romain;
+import personnages.Druide;
+
+public class Scénario {
+
+	public static void main(String[] args) {
+		/*Gaulois Asterix = new Gaulois ("Asterix",8);
+		Gaulois Obelix = new Gaulois ("Obelix",15);
+		Druide Panoramix = new Druide ("Panoramix",5,10);
+		Romain Minus = new Romain ("Minus",6);
+		Panoramix.parler("� Je vais aller pr�parer une petite potion...�");
+		Panoramix.booster(Obelix);
+		Obelix.parler("� Par Belenos, ce n'est pas juste !�");
+		Panoramix.booster(Asterix);
+		Asterix.parler("Bonjour � tous");
+		Minus.parler("UN GAU... UN GAUGAU...");
+		Asterix.frapper(Minus);
+		Asterix.frapper(Minus);
+		Asterix.frapper(Minus);*/
+	}
+}
